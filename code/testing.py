2c9902174c612631787e879261ea9b705a16229f
mfk <marius.fouquerand@gmail.com>
2019-07-25 10:39:02 -0700

    njn

--- code/testing.py
+++ code/testing.py
@@ -1,7 +1,8 @@
 import os
 OUTPUTS_DIR = os.getenv('VH_OUTPUTS_DIR', './outputs')
-path = os.path.join(OUTPUTS_DIR, 'ok.txt')
-f= open(path,"w+")
-for i in range(10):
-    f.write("This is line %d\r\n" % (i+1))
-f.close()
+path = os.path.join(OUTPUTS_DIR, 'ok.png')
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3], [4,5,6])
+plt.savefig(path)
+
