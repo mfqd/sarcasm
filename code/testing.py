628bab155e2fe6e9bc3368a58a175c9498b2d695
mfk <marius.fouquerand@gmail.com>
2019-07-25 10:37:40 -0700

    njn

--- code/testing.py
+++ code/testing.py
@@ -1,4 +1,7 @@
-f= open("/valohai/outputs/okok.txt","w+")
+import os
+OUTPUTS_DIR = os.getenv('VH_OUTPUTS_DIR', './outputs')
+path = os.path.join(OUTPUTS_DIR, 'ok.txt')
+f= open(path,"w+")
 for i in range(10):
     f.write("This is line %d\r\n" % (i+1))
-f.close() 
+f.close()
